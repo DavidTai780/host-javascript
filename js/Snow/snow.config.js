52720f7212b9fb040d29688e69729f27559790d6
DavidTai780 <taidavid780@gmail.com>
2020-05-18 18:39:41 +0800

    Update snow.config.js

--- js/Snow/snow.config.js
+++ js/Snow/snow.config.js
@@ -1,4 +1,10 @@
 document.write("<style>#Snow{position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 99999; background: rgba(125,137,95,0.1); pointer-events: none;}</style>");
 var d= new Date();
-var tag = d.getHours();
-document.write("<canvas id='Snow' width='100%' height='100%'></canvas><script src='//cdn.jsdelivr.net/gh/DavidTai780/host-javascript@master/js/Snow/snow-src.js?"+tag+"'></script>");
+var yr = d.getFullYear();
+var syr = yr.toString();
+var hr = d.getHours();
+var shr = hr.toString();
+var mn = d.getMinutes();
+var smn = mn.toString();
+var time = syr.concat(shr, smn);
+document.write("<canvas id='Snow' width='100%' height='100%'></canvas><script src='//cdn.jsdelivr.net/gh/DavidTai780/host-javascript@master/js/Snow/snow-src.js?"+time+"'></script>");
