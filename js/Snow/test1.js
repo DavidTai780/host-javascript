846eaa4ffeea111a46145e13af814e19b53eb15e
DavidTai780 <taidavid780@gmail.com>
2020-05-18 18:15:12 +0800

    Delete test1.js

--- js/Snow/test1.js
+++
@@ -1,4 +0,0 @@
-document.write("<style>#Snow{position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 99999; background: rgba(125,137,95,0.1); pointer-events: none;}</style>");
-var d= new Date();
-var tag = d.getHours();
-document.write("<canvas id='Snow' width='100%' height='100%'></canvas><script src='//cdn.jsdelivr.net/gh/DavidTai780/host-javascript@master/js/Snow/snow-src.js"+tag+"'></script>");
