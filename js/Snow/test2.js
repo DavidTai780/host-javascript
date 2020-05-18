8d9dfdbb4c0ec906cde06cbf9eef5109b8db9887
DavidTai780 <taidavid780@gmail.com>
2020-05-18 18:39:13 +0800

    Update test2.js

--- js/Snow/test2.js
+++ js/Snow/test2.js
@@ -1,6 +1,6 @@
 document.write("<style>#Snow{position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 99999; background: rgba(125,137,95,0.1); pointer-events: none;}</style>");
 var d= new Date();
-var yr = d.getYear();
+var yr = d.getFullYear();
 var syr = yr.toString();
 var hr = d.getHours();
 var shr = hr.toString();
